3b1b063f97ca0046b1ed5035350ee2bf8e0a1cfd
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-03-05 18:03:57 +0300

    refactor: use withTypes for typed Redux hooks

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import type { AppDispatch } from './configure-store';
 import type { RootState } from './configure-store';
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
