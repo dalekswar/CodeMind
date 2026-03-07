5f29a4f776beb46bdde70da8e399bbbe840af16a
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-03-07 17:48:55 +0300

    feat: add basic header layout for unauthorized user

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,3 +1,44 @@
+import { NavLink } from 'react-router-dom';
+import { Paths } from '../../app/router/paths';
+import styles from './header.module.css';
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.list}>
+          <li className={styles.logoItem}>
+            <NavLink to={Paths.ROOT} className={styles.logo}>
+              CodeMind
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={Paths.ABOUT_US}
+              className={({ isActive }) =>
+                `${styles.link} ${styles.aboutLink} ${isActive ? styles.active : ''}`
+              }
+            >
+              About Us
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={Paths.COURSES}
+              className={({ isActive }) =>
+                `${styles.link} ${styles.coursesLink} ${isActive ? styles.active : ''}`
+              }
+            >
+              Courses
+            </NavLink>
+          </li>
+          <li className={styles.joinItem}>
+            <NavLink to={Paths.REGISTER} className={`${styles.link} ${styles.joinLink} `}>
+              Join Now
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
